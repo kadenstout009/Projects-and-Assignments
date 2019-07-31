708cca1dc79f5476bb061ec58764424850796133
kadenstout009 <kstout@student.neumont.edu>
2019-07-31 12:38:39 -0600

    Added two files to main repository.

---
+++ JavaScriptPractice/IF_ELSE_statements/ifelse.js
@@ -0,0 +1,26 @@
+// looking for a thrill ...
+// ...
+
+let selectedThrill = "gaming";
+
+// pertains to gaming
+if(selectedThrill === "gaming") {
+    // if thrill is gaming
+    // do this
+    console.log("You selected gaming, what a thrill!")
+} else {
+    // if not gaming
+    // do this
+    console.log("You did NOT select gaming.")
+}
+
+// pertains to snowboarding
+if(selectedThrill === "snowboarding") {
+    // if thrill is snowboarding
+    // do this
+    console.log("You selected snowboarding, what a thrill!")
+} else {
+    // if not snowboarding
+    // do this
+    console.log("You did NOT select snowboarding.")
+}
